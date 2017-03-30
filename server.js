245f7548f8eda0052d9be88a0357da957b842df5
Juan Reyes <sbsreyes.js@gmail.com>
2017-03-29 21:31:32 -0500

    se establece la conexion con el servidor a través de expres

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res,) =>{
-  
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+app.get('/', (req, res) =>{
+  res.send('hola mundo')
+})
+
+app.listen(3000, (err)=>{
+  if(err){
+    console.log('hubo un error en la conexion');
+  }
+  else {
+    console.log('conexion establecida, escuchando en el puerto 3000')
+  }
+
 })
